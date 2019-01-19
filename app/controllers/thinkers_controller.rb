c1f0700dec94b1c3a9ad746eaa4a01cb484cad92
danny <danixeka@gmail.com>
2019-01-19 15:52:58 +1100

    completed views for thinkers

--- app/controllers/thinkers_controller.rb
+++ app/controllers/thinkers_controller.rb
@@ -1,16 +1,40 @@
 class ThinkersController < ApplicationController
-  def _form
-  end
-
-  def edit
-  end
-
   def index
-  end
-
-  def new
+    @thinkers = Thinker.all
   end
 
   def show
+    @thinker = Thinker.find params[:id]
   end
+
+  def new
+    @thinker = Thinker.new
+  end
+
+  def create
+    thinker = Thinker.create thinker_params
+    redirect_to thinker_path(thinker.id)
+  end
+
+  def edit
+    @thinker = Thinker.find params[:id]
+  end
+
+  def update
+    thinker = Thinker.find params[:id]
+    thinker.update thinker_params
+    redirect_to thinker_path(thinker.id)
+  end
+
+  def destroy
+    thinker = Thinker.find params[:id]
+    thinker.destroy
+    redirect_to thinkers_path
+  end
+
+  private
+  def thinker_params
+    params.require(:thinker).permit(:name, :birth, :death, :nationality, :era, :image, :quote)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
-  get 'fields/_form'
-  get 'fields/edit'
-  get 'fields/index'
-  get 'fields/new'
-  get 'fields/show'
-  get 'theories/_form'
-  get 'theories/edit'
-  get 'theories/index'
-  get 'theories/new'
-  get 'theories/show'
-  get 'thinkers/_form'
-  get 'thinkers/edit'
-  get 'thinkers/index'
-  get 'thinkers/new'
-  get 'thinkers/show'
   root :to => 'pages#home'
   resources :users, :only => [:new, :create, :index]
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
+
+  resources :thinkers
+  resources :theories
+  resources :fields
 end
